957cecbbff9a589a13477ced8b28355edbebfa4c
Rakesh Kumar Nayak <rakravmca@gmail.com>
2015-10-28 10:56:44 +0530

    Added new project for Business layer

--- SampleWebsite.Business/Properties/AssemblyInfo.cs
+++ SampleWebsite.Business/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SampleWebsite.BusinessLogic")]
+[assembly: AssemblyTitle("SampleWebsite.Business")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SampleWebsite.BusinessLogic")]
+[assembly: AssemblyProduct("SampleWebsite.Business")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d09910ee-6d02-4058-9dc6-c97ce4926974")]
+[assembly: Guid("073cd71e-b7fd-4a1c-8106-4a4ad0191c74")]
 
 // Version information for an assembly consists of the following four values:
 //
